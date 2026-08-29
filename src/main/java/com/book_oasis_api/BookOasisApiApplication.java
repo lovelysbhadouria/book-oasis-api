12c98d4ef3fe84c78f80190c4af4cc3365c4026e
Lovely Bhadouria <lovelysbhadouria1999@gmail.com>
2026-08-29 17:17:33 +0100

    Initial Spring Boot project setup with Web, JPA, H2, Validation, Lombok

---
+++ src/main/java/com/book_oasis_api/BookOasisApiApplication.java
@@ -0,0 +1,13 @@
+package com.book_oasis_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookOasisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookOasisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/book_oasis_api/BookOasisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book_oasis_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookOasisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
