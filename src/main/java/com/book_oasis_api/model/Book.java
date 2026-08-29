843ce133062a9c41e98a3a4c4198ca994449bdf2
Lovely Bhadouria <lovelysbhadouria1999@gmail.com>
2026-08-29 22:13:13 +0100

    Add validation constraints to Book entity

--- src/main/java/com/book_oasis_api/model/Book.java
+++ src/main/java/com/book_oasis_api/model/Book.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,9 +20,15 @@ public class Book {
     
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
-
 private Long id;
+
+@NotBlank(message = "Title is required")
 private String title;
+
+@NotBlank(message = "Author is required")
 private String author;
+
+@NotBlank(message = "Publication year is required")
+@Max(value = 2026, message = "Publication year cannot be in the future")
 private Integer publicationYear;
 }
